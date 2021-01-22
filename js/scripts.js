391093dc9f2199a923ff37656e8a1fdeebcbd69d
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-22 11:30:59 -0800

    add BL for 1 and else and return for product

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,31 @@ $(document).ready(function() {
   $("#neighborhood").submit(function(event) {
     event.preventDefault();
     const input = $("#numberInput").val();
+    temp(input)
   });
 });
 
 function temp(input) {
   let inputAr = [];
   let stringer = [];
-  for (let i = 0; i <= input; i++);
-  inputAr.push(i);
-  stringer = inputAr.join(" ");
-  splity = stringer.split(" ");
-  if (splity[i].includes("3")) { 
-    outtyput.push("Won't you be my neighbor?")
+  let splity = [];
+  let outtyput = [];
+  for (let i = 0; i <= input; i++) {
+    inputAr.push(i);
+    stringer = inputAr.join(" ");
+    splity = stringer.split(" ");
+    if (splity[i].includes("3")) { 
+      outtyput.push("Won't you be my neighbor?")
+    }
+    else if (splity[i].includes("2")) {
+      outtyput.push("Boop!")
+    }
+    else if (splity[i].includes("1")) {
+      outtyput.push("Beep!")
+    } else {
+      outtyput.push(i);
+      // console.log(outtyput)
+    }
   }
-  else if (splity[i].includes("2")) {
-    outtyput.push("Boop!")
-  }
+  return product
 };
