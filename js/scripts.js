7ecb74a917b8bede2493a049573bd9c5d0469f44
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-01-22 11:00:03 -0800

    add businesslogic for 3 and 2 start of if else

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,18 @@ $(document).ready(function() {
     const input = $("#numberInput").val();
   });
 });
+
+function temp(input) {
+  let inputAr = [];
+  let stringer = [];
+  for (let i = 0; i <= input; i++);
+  inputAr.push(i);
+  stringer = inputAr.join(" ");
+  splity = stringer.split(" ");
+  if (splity[i].includes("3")) { 
+    outtyput.push("Won't you be my neighbor?")
+  }
+  else if (splity[i].includes("2")) {
+    outtyput.push("Boop!")
+  }
+};
